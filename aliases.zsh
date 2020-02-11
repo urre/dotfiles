cb5863d7f4620c501d29d820f1a6a1ac2d1b6c28
Urban Sandén <hej@urre.me>
2020-02-11 21:35:21 +0100

    Add git aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,12 +2,31 @@ alias cls="clear"
 alias size="du -k -a | sort -n"
 alias iip="ifconfig | grep inet"
 alias eip="curl icanhazip.com"
+alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 
-# Git
-alias gitlog="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias gitlogtoday="git log --author=urbansanden --no-merges --since="6am" --pretty=oneline --abbrev-commit"
-alias gitlog10="git log --pretty=oneline --abbrev-commit -10"
+# Git log
+alias gl="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+# Git log today
+alias glt="git log --no-merges --since="6am" --pretty=oneline --abbrev-commit"
+
+# Git log this week
+alias glw="git shortlog --all --no-merges --author $(git config user.email) --since "1 week ago""
+
+# Git status
+alias gs="git status -s"
+
+# Copy current git branch name in clipboard
 alias gb="git branch | grep '^\*' | cut -d' ' -f2 | pbcopy"
+
+# Check total git commits across all branches
+alias gt="git rev-list --all --count"
+
+# Total git branches
+alias gbt="git branch | wc -l"
+
+# Number of commits per author on all branches
+alias gct="git shortlog -s -n --all --no-merges --since='1 Jan, 2019'"
 
 # Docker
 alias dockerkill='docker kill $(docker ps -q)'
@@ -32,7 +51,7 @@ function port() {
   lsof -nP -iTCP:"$1"
 }
 
-# Merge two images side by side, open in Preview
+# Merge two images side by side, then open in Preview
 function mont() {
   montage -background '#f2f2f2' -geometry 100% ~/desktop/"$1" ~/desktop/"$2" ~/desktop/merged.png;
   open -a Preview ~/desktop/merged.png
