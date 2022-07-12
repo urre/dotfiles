7e82b98032063f771a73552715172dae477d3c06
Urban Sanden <hej@urre.me>
2022-07-12 20:48:13 +0200

    PATH fixes

--- path.zsh
+++ path.zsh
@@ -1,11 +1,11 @@
 # Python
-export PATH="/usr/local/opt/python@3/libexec/bin:$PATH";
+export PATH="/opt/homebrew/opt/python@3.9/libexec/bin:$PATH";
 
-# Composer
-export PATH=/Users/urbansanden/.composer/vendor/bin:$PATH
+# Poetry
+export PATH="/$HOME/.poetry/bin:$PATH";
 
-# Java
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+# Homebrew M1
+export PATH="/opt/homebrew/bin:$PATH"
 
 # Add java bin folder in PATH system environment variable value.
 export PATH=$PATH:$JAVA_HOME/bin
